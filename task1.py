57890d8211f8e94df45563bd97588c960473e041
George Wanga <georgewanga@users.noreply.github.com>
2018-08-01 22:30:13 +0300

    created a valriable belowFive with only the elemets of ourList that are below 5 in test1

--- task1.py
+++ task1.py
@@ -5,5 +5,9 @@ count = 0
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
-    
-ourList
+
+print(ourList)
+
+# list comprehension for elemets of **ourList** that are below 5
+belowFive = [item for item in ourList if item<5]
+print(belowFive)
